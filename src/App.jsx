a074d7857b1f0dd51804ba7a573425d70362f5d0
pame_latorre <pamelatorre33@gmail.com>
2023-09-27 10:35:04 -0300

    descargamos MUI

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,21 @@
+import { ThemeProvider } from "@emotion/react";
 import { Navbar } from "./components/layout/navbar/Navbar";
 import { Home } from "./components/pages/home/Home";
 import ItemListContainer from "./components/pages/itemListContainer/ItemListContainer";
 import { Login } from "./components/pages/login/Login";
+import { customTheme } from "./themeConfig";
 
 function App() {
   let saludo = "Hola! Como estas?";
   return (
-    <div>
-      <Navbar />
-      {/* <Home />
+    <ThemeProvider theme={customTheme}>
+      <div>
+        <Navbar />
+        {/* <Home />
       <Login /> */}
-      <ItemListContainer saludo={saludo} />
-    </div>
+        <ItemListContainer saludo={saludo} />
+      </div>
+    </ThemeProvider>
   );
 }
 
---
+++ src/themeConfig.js
@@ -0,0 +1,14 @@
+import { createTheme } from "@mui/material";
+
+export const customTheme = createTheme({
+  palette: {
+    primary: {
+      main: "#c19c9c",
+      secondary: "#957d7c",
+    },
+    secondary: {
+      main: "#d0c3c1",
+      secondary: "#f6f2f0",
+    },
+  },
+});
